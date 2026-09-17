e11d2988c25a22e3e6c8b5d43396e7c6a53b01cb
Obscrum <194935315+Xeakaes@users.noreply.github.com>
2026-09-17 17:38:30 +0300

    Add missing Python SDK package marker

---
+++ sdk/__init__.py
@@ -0,0 +1,6 @@
+"""Screen Control Python SDK"""
+
+from .screen_control import ScreenControl, SC, ScreenControlError
+
+__version__ = "1.0.0"
+__all__ = ["ScreenControl", "SC", "ScreenControlError"]
